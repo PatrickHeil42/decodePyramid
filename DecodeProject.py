208f2dfc5728482ce83bb2ee02da45b45c48d027
Pheil9220 <91139061+PatrickHeil42@users.noreply.github.com>
2024-02-21 15:09:49 -0600

    Update DecodeProject.py

--- DecodeProject.py
+++ DecodeProject.py
@@ -4,39 +4,60 @@
 import re
 from time import sleep
 
-def count(file):
+def count():
     
-    print("Decoding " + pathToFile)
-    myFile = open(pathToFile, 'r')
-    text = myFile.read(myFile) 
+    pathToFile = "coding_qual_input.txt"
+    print("Counting: " + pathToFile)
+    f = open("coding_qual_input.txt", "r")
+    #print(f.readlines())
+    text = f.readlines()
     numberOfLines = 0
-    for character in text:
-        if character == "\n":
-            numberOfLines = numberOfLines + 1
-    return "This input has " + numberOfLines + "."
-def read(file):
-    myFile = open(pathToFile, 'r')
-    text = myFile.read(myFile)
-    lines = myFile.readlines()
+    for line in text:
+            numberOfLines += 1
+      
+    print("This input has " + str(numberOfLines) + " words in total.")
+    return numberOfLines
+
+def read():
+    pathToFile = "coding_qual_input.txt"
+    f = open("coding_qual_input.txt", "r")
+    text = f.readlines()
+    i=0
+    numberOfLines = count()
+    messageList[] = 
+    while i < numberOfLines:
+        
+
+        
     print(lines)
-     ##for character in text:
-      ##  if character == "\n":
-      ##       print
+    
     return lines
 '''     
 f(n)= 0.5 * n *(n+1)
 with n = pyramid level +1. 
 '''
-def decode(fileName):
-        print(numberOfLines = count(fileName))
-        print(fileText = read(fileName))
-        decoded_words = []
+'''
+def orderList(unorderedList[]) :
+    orderedList = []
+    for i : unorderedList[]
+        
+    
+    return orderedList
+'''
+def decode():
+        pathToFile = "coding_qual_input.txt"
+        count()
+        wordListUnordered = read()
+        #orderedList = orderList(wordListUnordered)
         message = "test"
         ##message.append("Test")
-        for i, line in enumerate(fileText):
-            pairs = line.strip().split()
+        #for i, line in enumerate(fileText):
+        #pairs = line.strip().split()
+        print(message)
         return message
         ##def decode(file)
-pathToFile = "c:\\Users\\Pat\\Desktop\\ai.job\\coding_qual_input.txt"
+#pathToFile = "c:\\Users\\Pat\\Desktop\\ai.job\\coding_qual_input.txt"
+pathToFile = "coding_qual_input.txt"
+
 #file = open(pathToFile, 'r')
-print(decode(pathToFile))
+decode()
